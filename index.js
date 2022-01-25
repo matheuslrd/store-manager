ed0b975d14a48317172b53bd8cc3469196dafb4c
Matheus Laurindo <matheus104521@gmail.com>
2022-01-25 16:10:41 -0300

    Cria o servidor e ativa

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
