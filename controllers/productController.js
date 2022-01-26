721399ea1de923cee0db61a0825e51239482a371
Matheus Laurindo <matheus104521@gmail.com>
2022-01-26 14:12:37 -0300

    Adiciona validacao do tamanho do nome do produto

--- controllers/productController.js
+++ controllers/productController.js
@@ -7,6 +7,10 @@ const validateName = (req, res, next) => {
     return res.status(400).json({ message: '"name" is required' });
   }
 
+  if (name < 5) {
+    return res.status(422).json({ message: '"name" length must be at least 5 characters long' });
+  }
+
   next();
 };
 
