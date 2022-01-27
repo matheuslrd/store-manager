1372ac4aea100899bde6b455e00e3f6ad788c3e7
Matheus Laurindo <matheus104521@gmail.com>
2022-01-27 08:37:46 -0300

    Adiciona validacao caso um id invalido seja passado e conclui requisito 2

--- controllers/productController.js
+++ controllers/productController.js
@@ -54,10 +54,13 @@ const getAll = async (_req, res) => {
 
 const getById = async (req, res) => {
   let { id } = req.params;
-
   id = Number(id);
 
   const product = await Product.getById({ id });
+
+  if (!product) {
+    return res.status(404).json({ message: 'Product not found' });
+  }
 
   return res.status(200).json(product);
 };
