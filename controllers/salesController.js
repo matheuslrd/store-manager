7543b798a98d3561120a65d853b7f6424f78972a
Matheus Laurindo <matheus104521@gmail.com>
2022-02-01 11:17:38 -0300

    Adiciona validacao de tipo de quantity

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -16,8 +16,14 @@ const validateQuantity = (req, res, next) => {
   const { body } = req;
 
   body.forEach((product) => {
-    if (!product.quantity) {
+    if (product.quantity === undefined) {
       return res.status(400).json({ message: '"quantity" is required' });
+    }
+
+    if (typeof product.quantity !== 'number' || product.quantity <= 0) {
+      return res.status(422).json(
+        { message: '"quantity" must be a number larger than or equal to 1' },
+      );
     }
   });
 
