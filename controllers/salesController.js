6acc063b3a6538cfe0aacd2e4cc6cf87d6ae3425
Matheus Laurindo <matheus104521@gmail.com>
2022-02-01 10:56:29 -0300

    Faz validacoes de product_id

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,5 +1,17 @@
 const SalesService = require('../services/salesService');
 
+const validateProductId = (req, res, next) => {
+  const { body } = req;
+
+  body.forEach((product) => {
+    if (!product.product_id) {
+      return res.status(400).json({ message: '"product_id" is required' });
+    }
+  });
+
+  next();
+};
+
 const createSales = async (req, res) => {
   const { body } = req;
 
@@ -11,4 +23,5 @@ const createSales = async (req, res) => {
 
 module.exports = {
   createSales,
+  validateProductId,
 };
--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.route('/products/:id')
   .delete(Product.deleteProduct);
 
 app.route('/sales')
-  .post(SalesController.createSales);
+  .post(SalesController.validateProductId, SalesController.createSales);
 
 app.route('/products')
   .post(Product.validateName, Product.findByName, Product.validateQuantity, Product.create)
